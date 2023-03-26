a192db6fceb0db037f62f6e20293d59bc35daf02
Kelvin Mendes de Araújo <65052645+kelvin-had1@users.noreply.github.com>
2023-03-25 23:59:34 -0300

    fix: referenced libraries

--- src/modules/repositories/BaseRepository.java
+++ src/modules/repositories/BaseRepository.java
@@ -6,13 +6,14 @@ public class BaseRepository extends ConnectionDatabase {
 	public ResultSet read(String query) throws SQLException{
 		Statement stmt = getConnection().createStatement();
 		ResultSet result = stmt.executeQuery(query);
-		
+		getConnection().close();
 		return result;
 	}
 	
 	public boolean execute(String query) throws SQLException{
 		Statement stmt = getConnection().createStatement();
 		int result = stmt.executeUpdate(query);
+		getConnection().close();
 		
 		if(result == 1) {
 			return true;
